bef0e3ac2e4e05204847ffa7b3dc5a6858c764fb
tejash098 <jaitej123@gmail.com>
2025-12-20 12:53:46 +0530

    feat: Create App component to render ProjectApp.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import ProjectApp from './Components/ProjectApp'
 import './App.css'
-import Gitapifetch from './Components/Projects/Gitapifetch/Gitapifetch'
 function App() {
   return (
     <>
       <ProjectApp />
-      {/* <Gitapifetch /> */}
     </>
   )
 }
