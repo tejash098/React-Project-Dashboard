cb47b0b34469d3533e30dff4cc9b831a68cdc329
Tejash kumar singh <jaitej123@gmail.com>
2026-02-13 18:27:16 +0530

    feat: adding Expense Manger

--- src/Components/Projects/Expense-Manager/ExpenseManager.jsx
+++ src/Components/Projects/Expense-Manager/ExpenseManager.jsx
@@ -2,22 +2,28 @@ import React, { useState, useEffect } from "react";
 import "./ExpenseManager.css";
 import { Category, Description } from "@mui/icons-material";
 const ExpenseManager = () => {
-  const [ExpenseList, setExpenseList] = useState([])
+  const [ExpenseList, setExpenseList] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [Budget, setBudget] = useState(0);
+  const [AmountLeft, setAmountLeft] = useState(0);
 
   const [ExpenseData, setExpenseData] = useState({
-    Description : "",
-    Category : "",
-    date : "",
+    Title: "",
+    Description: "No Description",
+    Category: "",
+    Date: "",
   });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setExpenseData({...ExpenseData, [name] : value});
+    setExpenseData({ ...ExpenseData, [name]: value });
   };
 
-  const handleSubmit = () => {
-    setExpenseList(...ExpenseList, ExpenseData)
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newExpenseData = ExpenseData;
+    setExpenseList([...ExpenseList, newExpenseData]);
+  };
 
   return (
     <>
@@ -34,43 +40,94 @@ const ExpenseManager = () => {
             <p>Amount Left</p>
           </div>
         </div>
-        <div className="input-form">
-          <form action="" onSubmit={handleSubmit}>
-            <label htmlFor="Description">Description</label>
-            <input
-              type="text"
-              name="Description"
-              id="Description"
-              onChange={handleChange}
-            />
-            <label htmlFor="Category">category</label>
-            <select name="Category" id="Category">
-              <option value="" disabled selected>
-                Select Category
-              </option>
-              <option value="shopping">shopping</option>
-              <option value="">Movies</option>
-              <option value="">Education</option>
-              <option value="">Miscellaneous</option>
-            </select>
-            <label htmlFor="date">date</label>
-            <input type="date" name="date" id="date" />
-            <input type="submit" placeholder="Add" />
-          </form>
-        </div>
-        <div className="expense-data">
-          <table>
-            <thead>
-              <tr>
-                <td>Sl No:</td>
-                <td>Description</td>
-                <td>Category</td>
-                <td>Date</td>
-                <td>Action</td>
-              </tr>
-            </thead>
-            <tbody></tbody>
-          </table>
+        <div className="grid-box">
+          <div className="input-form">
+            <form action="" onSubmit={handleSubmit}>
+              <label htmlFor="title">
+                Title <span>*</span>{" "}
+              </label>
+              <input
+                type="text"
+                id="title"
+                name="Title"
+                onChange={handleChange}
+                required
+              />
+              <label htmlFor="Description">Description</label>
+              <input
+                type="text"
+                name="Description"
+                id="Description"
+                onChange={handleChange}
+              />
+              <label htmlFor="Category">
+                Category <span>*</span>{" "}
+              </label>
+              <select
+                name="Category"
+                id="Category"
+                onChange={handleChange}
+                defaultValue="1"
+                required
+              >
+                <option value="1" disabled>
+                  Select Category
+                </option>
+                <option value="Shopping">Shopping</option>
+                <option value="Movies">Movies</option>
+                <option value="Education">Education</option>
+                <option value="Food">Food</option>
+                <option value="Transport">Transport</option>
+                <option value="Health">Health</option>
+                <option value="Entertainment">Entertainment</option>
+                <option value="Miscellaneous">Miscellaneous</option>
+              </select>
+              <label htmlFor="date">
+                date <span>*</span>{" "}
+              </label>
+              <input
+                type="date"
+                name="Date"
+                id="date"
+                onChange={handleChange}
+                required
+              />
+              <input type="submit" value="Add" />
+            </form>
+          </div>
+          <div className="expense-data">
+            <table>
+              <thead>
+                <tr>
+                  <td>Sl No:</td>
+                  <td>Title</td>
+                  <td>Description</td>
+                  <td>Category</td>
+                  <td>Date</td>
+                  <td>Action</td>
+                </tr>
+              </thead>
+              <tbody>
+                {ExpenseList.length !== 0 &&
+                  ExpenseList.map((elem, idx) => {
+                    const { Title, Description, Category, Date } = elem;
+                    return (
+                      <tr key={Title}>
+                        <td>{idx + 1}</td>
+                        <td>{Title}</td>
+                        <td>{Description}</td>
+                        <td>{Category}</td>
+                        <td>{Date}</td>
+                        <td className="action-buttons">
+                          <button className="view">View</button>
+                          <button className="delete">Delete</button>
+                        </td>
+                      </tr>
+                    );
+                  })}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </>
